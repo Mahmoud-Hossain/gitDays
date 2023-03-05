10f6cdcc9dad6b6c8a13e6bf68e896f58a057955
Abdulrahman alaa <aali@blinkapp.net>
2023-03-05 12:14:47 +0200

    another hello world

--- gitDay/ViewController.swift
+++ gitDay/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         print("First Intial commit ")
         print("Hello world!")
+        print(" another hello world")
     }
 
 
