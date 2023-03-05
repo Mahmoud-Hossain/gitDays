4c23e974e0b53d7344169365e546b4863b8c25a3
Abdulrahman alaa <aali@blinkapp.net>
2023-03-05 11:40:13 +0200

    Hello world! commit

--- gitDay/ViewController.swift
+++ gitDay/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("First Intial commit ")
+        print("Hello world!")
     }
 
 
